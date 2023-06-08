b8871206efb87d2a55bdf33cdcbe4493af9a05f1
Joao Batista Lopes Filho <batistafilho.lopes@gmail.com>
2023-06-08 21:21:04 +0200

    Commit NavBar

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import MyNavBar from './components/NavBar/MyNavBar.jsx';
+
+function App() {
+  return (
+    <div className="App">
+     <MyNavBar/>
+    </div>
+  );
+}
+
+export default App;
