a13eae1c884e4a54db98cde909a84ce8836ce67d
Joao Batista Lopes Filho <batistafilho.lopes@gmail.com>
2023-06-09 01:27:05 +0200

    Components & Banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
+import Banner from './components/Banner/Banner';
 import MyNavBar from './components/NavBar/MyNavBar.jsx';
 
 function App() {
   return (
     <div className="App">
      <MyNavBar/>
+     <Banner/>
     </div>
   );
 }
