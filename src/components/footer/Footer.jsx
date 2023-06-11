a1a79cf159ff97e20c5946ffc9821c0186650967
Joao Batista Lopes Filho <batistafilho.lopes@gmail.com>
2023-06-12 00:17:23 +0200

    Footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer__container container">
+
+        <div className="footer__social">
+          <a
+            href="https://www.instagram.com/joaofilho92/"
+            className="footer__social-link"
+            target="_blank"
+          >
+            <i class="bx bxl-instagram"></i>
+          </a>
+
+          <a
+            href="https://github.com/joaofilho92"
+            className="footer__social-link"
+            target="_blank"
+          >
+            <i class="bx bxl-linkedin"></i>
+          </a>
+
+          <a
+            href="https://www.linkedin.com/in/joao-batista-sviluppatorefrontend/?locale=it_IT"
+            className="footer__social-link"
+            target="_blank"
+          >
+            <i class="bx bxl-github"></i>
+          </a>
+        </div>
+        <span className='footer__copy'>&#169; 2023 Joao Batista Lopes Filho. All Rights reserved.</span>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer
